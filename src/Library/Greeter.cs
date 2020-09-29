f243a345f9276b9401abb9f0f46cc11abe491296
gonzalo prampini <gonzaloprampini@gmail.com>
2020-09-29 14:00:15 -0300

    commit

--- src/Library/Greeter.cs
+++ src/Library/Greeter.cs
@@ -3,29 +3,10 @@
 namespace Library
 {
     public class Greeter
+    {   
+
+    public void SayHiToTheWorld()
     {
-        private Greeter()
-        {
-            // Intentionally left blank
-        }
-
-        private static Greeter instance;
-
-        public static Greeter Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Greeter();
-                }
-
-                return instance;
-            }
-        }
-
-        public void SayHiToTheWorld()
-        {
             Console.WriteLine("Hello World!");
         }
     }
